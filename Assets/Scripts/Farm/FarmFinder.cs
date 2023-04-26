db893107faf9622f1b10153346f206dff3873267
Roman <125725530+RomanGleizer@users.noreply.github.com>
2023-04-26 23:20:39 +0500

    Find farm

--- Assets/Scripts/Farm/FarmFinder.cs
+++ Assets/Scripts/Farm/FarmFinder.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FarmFinder : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.GetComponent<Farm>())
             print("Ферма найдена! ");
