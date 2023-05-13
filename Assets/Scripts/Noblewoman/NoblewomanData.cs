736b30ce9bbd2b0ba1d405c5cf022c9ea2875672
Roman <125725530+RomanGleizer@users.noreply.github.com>
2023-05-13 11:21:29 +0500

    Noblewoman correct move

---
+++ Assets/Scripts/Noblewoman/NoblewomanData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class NoblewomanData
+{
+    public Vector3 Position;
+    public Transform Target;
+    public int CurrentPoint;
+    public bool IsPlayerMoveLeft;
+    public bool IsPlayerMoveRight;
+    public bool IsPlayerMoveUp;
+    public bool IsPlayerMoveDown;
+}
--- Assets/Scripts/Noblewoman/NoblewomanMover.cs
+++ Assets/Scripts/Noblewoman/NoblewomanMover.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 
 public class NoblewomanMover : MonoBehaviour
@@ -8,6 +9,7 @@ public class NoblewomanMover : MonoBehaviour
 
     private Transform[] _points;
     private int _currentPoint;
+    private Transform _target;
 
     private void Start()
     {
@@ -18,10 +20,10 @@ public class NoblewomanMover : MonoBehaviour
 
     private void Update()
     {
-        var target = _points[_currentPoint];
-        transform.position = Vector3.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
+        _target = _points[_currentPoint];
+        transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
     
-        if (transform.position == target.position)
+        if (transform.position == _target.position)
         {
             _currentPoint++;
             if (_currentPoint >= _points.Length) _currentPoint = 0;
@@ -30,22 +32,53 @@ public class NoblewomanMover : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<MoveUpPoint>())
-            SwitchBoolStates("IsMoveUp", "IsMoveLeft");
-
-        if (collision.GetComponent<MoveRightPoint>())
-            SwitchBoolStates("IsMoveRight", "IsMoveUp");
-
-        if (collision.GetComponent<MoveDownPoint>())
-            SwitchBoolStates("IsMoveDown", "IsMoveRight");
-
-        if (collision.GetComponent<MoveLeftPoint>())
-            SwitchBoolStates("IsMoveLeft", "IsMoveDown");
+        if (collision.GetComponent<MoveUpPoint>()) SwitchBoolStates("IsMoveUp", "IsMoveLeft");
+        if (collision.GetComponent<MoveRightPoint>()) SwitchBoolStates("IsMoveRight", "IsMoveUp");
+        if (collision.GetComponent<MoveDownPoint>()) SwitchBoolStates("IsMoveDown", "IsMoveRight");
+        if (collision.GetComponent<MoveLeftPoint>()) SwitchBoolStates("IsMoveLeft", "IsMoveDown");
     }
 
     private void SwitchBoolStates(string first, string second)
     {
         _animator.SetBool(first, true);
         _animator.SetBool(second, false);
+    }
+
+    public void SaveNobleWomanData()
+    {
+        var data = new NoblewomanData();
+
+        data.Position = transform.position;
+        data.Target = _target;
+        data.CurrentPoint = _currentPoint;
+
+        data.IsPlayerMoveLeft = _animator.GetBool("IsMoveLeft");
+        data.IsPlayerMoveRight = _animator.GetBool("IsMoveRight");
+        data.IsPlayerMoveUp = _animator.GetBool("IsMoveUp");
+        data.IsPlayerMoveDown = _animator.GetBool("IsMoveDown");
+
+        var json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(
+            Application.dataPath + "/NoblewomanData.json",
+            json,
+            encoding: System.Text.Encoding.UTF8);
+    }
+
+    public void LoadNobleWomanData()
+    {
+        var json = File.ReadAllText(
+            Application.dataPath + "/NoblewomanData.json",
+            encoding: System.Text.Encoding.UTF8);
+
+        var data = JsonUtility.FromJson<NoblewomanData>(json);
+
+        if (data.IsPlayerMoveRight) _animator.Play("NoblewomanMoveRight");
+        if (data.IsPlayerMoveLeft) _animator.Play("NoblewomanMoveLeft");
+        if (data.IsPlayerMoveUp) _animator.Play("NoblewomanMoveUp");
+        if (data.IsPlayerMoveDown) _animator.Play("NoblewomanMoveDown");
+
+        _target = data.Target;
+        _currentPoint = data.CurrentPoint;
+        transform.position = data.Position;
     }
 }
