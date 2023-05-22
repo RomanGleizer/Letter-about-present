a4005cd2e5c6d7e7b4a282697db98daaeca49488
ilyusha111 <130544310+ilyusha111@users.noreply.github.com>
2023-05-22 21:04:00 +0500

    Update FarmMenuHandler.cs

--- Assets/Scripts/Canvas Scripts/FarmMenuHandler.cs
+++ Assets/Scripts/Canvas Scripts/FarmMenuHandler.cs
@@ -13,22 +13,38 @@ public class FarmMenuHandler : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.U) 
-            && !_loadGameCanvas.activeSelf && !_pauseMenu.activeSelf && !_stockMenu.activeSelf
-            && !_vegetableShop.activeSelf && !_boostShop.activeSelf && _isMenuOpen == false)
+        if (Input.GetKeyDown(KeyCode.U) && IsInsideFarmArea() && !_loadGameCanvas.activeSelf && !_pauseMenu.activeSelf && !_stockMenu.activeSelf && !_vegetableShop.activeSelf && !_boostShop.activeSelf && !_isMenuOpen)
         {
             _isMenuOpen = true;
             _farmMenu.SetActive(true);
         }
-        else if (Input.GetKeyDown(KeyCode.U) && _isMenuOpen == true)
+        else if ((Input.GetKeyDown(KeyCode.U) || !IsInsideFarmArea()) && _isMenuOpen)
         {
             _isMenuOpen = false;
             _farmMenu.SetActive(false);
         }
     }
 
+    private bool IsInsideFarmArea()
+    {
+        // Проверка, находится ли игрок в определенной области фермы.
+
+        Vector2 playerPosition = transform.position;
+        float xMin = -22f;
+        float xMax = -7f;
+        float yMin = -11f;
+        float yMax = 11f;
+
+        if (playerPosition.x >= xMin && playerPosition.x <= xMax && playerPosition.y >= yMin && playerPosition.y <= yMax)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
     public void CloseMenu(GameObject menu)
     {
-        menu.SetActive(false); 
+        menu.SetActive(false);
     }
 }
